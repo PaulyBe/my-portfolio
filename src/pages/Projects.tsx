c44f1c5c68d794e8dde603fe6f3786f629b767ba
Paul <paul.bellmann@gmail.com>
2025-08-07 16:20:45 +0200

    Projects Update

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -15,14 +15,14 @@ const projectsData: Project[] = [
   {
     id: 1,
     title: 'To Do-List',
-    description: 'A simple To Do List application, that allows users to add new tasks and delete old ones. Built with: HTML, CSS & JavaScript.',
+    description: 'A simple To Do List application, that allows users to add new tasks and delete old ones. Built with: HTML, CSS & JavaScript',
     icon: <CiCircleList size={80} color="#A78BFA" />,
     demoLink: 'https://paulybe.github.io/todolist/',
   },
   {
     id: 2,
     title: 'Card Game',
-    description: 'A card game to get to know people better, that displays random cards from a deck, with three different levels. Built with: React, Tailwind CSS & TypeScript.',
+    description: 'A card game to get to know people better, that displays random cards from a deck, with three different levels. Built with: React, Tailwind CSS & TypeScript',
     icon: <TbCards size={80} color="lightblue" />,
     demoLink: 'https://1000questions.site/',
   },
@@ -47,13 +47,62 @@ const Contact = () => {
     <section className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-green-100 pt-30 pb-16 px-4 sm:px-6 lg:px-8 pt-20">
       <div className="max-w-6xl mx-auto">
         <h2 className="text-4xl font-extrabold text-center text-gray-800 mb-12">
-          My Projects
+          Websites
         </h2>
+        <p className="text-lg text-gray-700 text-center max-w-2xl mx-auto mb-16">
+          Here are some of the websites I built for other people. I removed personal information and solely kept the frontend.
+        </p>
 
+        <div className="max-w-4xl mx-auto grid grid-cols-1 gap-16 pb-14">
+          <div className="bg-white rounded-xl shadow-xl border border-gray-200 overflow-hidden
+                         transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl flex flex-col">
+              <div className="p-6 flex flex-col flex-1">
+                <h3 className="text-xl font-bold text-gray-900 mb-3">Hairdresser Website</h3>
+                <p className="text-gray-700 text-base mb-4">A website I built for a client, that is a hairdresser. I decided to go for a single-page approach. The website should display some information about the shop, the employees and have its social media channels linked; as well as a form to book an appointment.</p>
+                <p className="text-gray-700 text-base mb-4 font-bold">The website is built in React with TypeScript and Tailwind CSS</p>
+                <div className="text-center">
+                <a
+                      href="https://paulybe.github.io/hairdresser_website/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="inline-flex items-center w-56 min-w-min px-4 py-2 bg-blue-800 text-white text-sm font-medium rounded-full
+                                 hover:bg-blue-700 transition-colors duration-200 shadow-md justify-center"
+                    >
+                      Check it out
+                    </a>
+                </div>
+              </div>
+          </div>
+        </div>
+
+        <div className="max-w-4xl mx-auto grid grid-cols-1 gap-16 pb-14">
+          <div className="bg-white rounded-xl shadow-xl border border-gray-200 overflow-hidden
+                         transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl flex flex-col">
+              <div className="p-6 flex flex-col flex-1">
+                <h3 className="text-xl font-bold text-gray-900 mb-3">Billing Website</h3>
+                <p className="text-gray-700 text-base mb-4">A website I built for a client, that offers billing services. The client wanted a website with several page with information about his business. Furthermore, the website should have a contact form. I created a website with informational sections, set up the contact form and optimized it for mobile use.</p>
+                <p className="text-gray-700 text-base mb-4 font-bold">The website is simply built with HTML, CSS and JavaScript</p>
+                <div className="text-center">
+                <a
+                      href="https://paulybe.github.io/billing_website/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="inline-flex items-center w-56 min-w-min px-4 py-2 bg-blue-800 text-white text-sm font-medium rounded-full
+                                 hover:bg-blue-700 transition-colors duration-200 shadow-md justify-center"
+                    >
+                      Check it out
+                    </a>
+                </div>
+              </div>
+          </div>
+        </div>
+
+        <h2 className="text-4xl font-extrabold text-center text-gray-800 mb-12">
+          Personal Projects
+        </h2>
         <p className="text-lg text-gray-700 text-center max-w-2xl mx-auto mb-16">
           Here are some of the projects I have been working on, to improve my coding skills.
         </p>
-
         <div className="max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-16">
           {projectsData.map((project) => (
             <div
@@ -69,7 +118,7 @@ const Contact = () => {
                     src={project.imageUrl}
                     alt={project.title}
                     className="h-24"
-                    onError={(e) => { e.currentTarget.src = 'https://placehold.co/400x300/CCCCCC/000000?text=Bild+nicht+gefunden'; }}
+                    onError={(e) => { e.currentTarget.src = 'https://placehold.co/400x300/CCCCCC/000000?text=Picture+not+found'; }}
                   />
                 ) : null}
               </div>
